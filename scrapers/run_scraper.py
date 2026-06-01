961f393d2589c7228ecc5eea94984ce565fa8824
Maddie Wong <maddie12@outlook.com>
2026-06-01 15:26:59 -0400

    more debugging

--- scrapers/run_scraper.py
+++ scrapers/run_scraper.py
@@ -3,43 +3,39 @@ from postgres_conn import PostgresDB
 import pandas as pd
 import os
 
-def main():
-    db = PostgresDB(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=int(os.getenv("DB_PORT", 5432))
-    )
-    db.connect()
 
-    scraper = FotMobScraper()
+db = PostgresDB(
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USER"),
+    password=os.getenv("DB_PASSWORD"),
+    port=int(os.getenv("DB_PORT", 5432))
+)
+db.connect()
 
-    LEAGUES = {
-        9227: "WSL",
-        9134:"NWSL",
-        9907: "Liga F",
-        9676: "Frauen Bundesliga",
-        9677: "Premiere League Feminine",
-        10178: "Serie A Femminile",
-    }
+scraper = FotMobScraper()
 
-    for league_id, league_name in LEAGUES.items():
-        print(f"Scraping league: {league_name}")
-        teams = scraper.get_teams_from_league(league_id)
-        team_player_map = {}
+LEAGUES = {
+    9227: "WSL",
+    9134:"NWSL",
+    9907: "Liga F",
+    9676: "Frauen Bundesliga",
+    9677: "Premiere League Feminine",
+    10178: "Serie A Femminile",
+}
 
-        for team_url in teams:
-            team_players = scraper.get_players_from_team(team_url)
-            team_player_map[team_url] = team_players
+for league_id, league_name in LEAGUES.items():
+    print(f"Scraping league: {league_name}")
+    teams = scraper.get_teams_from_league(league_id)
+    team_player_map = {}
 
-            for player_id in team_player_map[team_url]:
-                bio = scraper.get_player_bio(player_id)
-                season_overview = scraper.get_current_season_overview(player_id)
-                db.save_player_fotmob_data(bio, season_overview, season_stats_df=pd.DataFrame())
-    db.close()
-    scraper.close()
+    for team_url in teams:
+        team_players = scraper.get_players_from_team(team_url)
+        team_player_map[team_url] = team_players
 
-if __name__ == "__main__":
-    print("Processing...")
-    main()
+        for player_id in team_player_map[team_url]:
+            bio = scraper.get_player_bio(player_id)
+            season_overview = scraper.get_current_season_overview(player_id)
+            db.save_player_fotmob_data(bio, season_overview, season_stats_df=pd.DataFrame())
+db.close()
+scraper.close()
