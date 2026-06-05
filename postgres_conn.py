975de00e5e88bfe42c33e91d4962c095420ff222
Maddie Wong <maddie12@outlook.com>
2026-06-05 08:56:57 -0400

    fix for last commit

--- postgres_conn.py
+++ postgres_conn.py
@@ -62,14 +62,14 @@ class PostgresDB:
     def save_raw_json(self, player_id, raw_json):
         createdate = datetime.now()
         insert_query = """
-            INSERT INTO fotmob_data.raw_player_overview (player_id, createdate, Json(raw_json))
+            INSERT INTO fotmob_data.raw_player_overview (player_id, createdate, raw_json)
             VALUES (%s, %s, %s)
             ON CONFLICT (player_id)
             DO UPDATE SET 
                 createdate = EXCLUDED.createdate,
                 raw_json = EXCLUDED.raw_json
         """
-        self.execute(insert_query, (player_id, createdate, raw_json))
+        self.execute(insert_query, (player_id, createdate, Json(raw_json)))
         self.commit()
     
     def save_player_fotmob_data(self, bio_df, season_overview_df, season_stats_df):
